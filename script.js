8c38845ff367b4bebe63f94cef0a8bce76e0d07d
Lucas Fernandes Andrade <lucasfeandrade@gmail.com>
2017-02-27 01:10:19 -0300

    Changing colours randomly

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function changeColors() {
+    let red = Math.round(Math.random()*255);
+    let blue = Math.round(Math.random()*255);
+    let green = Math.round(Math.random()*255);
 
+    $('body').animate({
+        backgroundColor: `rgb(${red},${green},${blue})`
+    },600);
+    $('i').animate({
+        color: `rgb(${red},${green},${blue})`
+    },600);
 }
 
 
@@ -12,11 +21,12 @@ function updateQuote() {
             data: { "cat": "movies"},
             dataType: 'json',
             success: function(data){
-                var author = data.author;
-                var quote = data.quote;
+                let author = data.author;
+                let quote = data.quote;
                 console.log(author);
                 $('#quote-author').text("- " + author);
                 $('#quote-content').text(quote);
+                changeColors();
             },
             beforeSend: function(xhr) {
                 xhr.setRequestHeader("X-Mashape-Authorization", "FFvHWpFk7emshbyd7Wmx9uP6wB92p1Zsadejsnpg6SacZSTUU6");
